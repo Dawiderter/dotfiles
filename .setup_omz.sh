1757398a1d32f4963e53a197449c9d00dbe1bed5
dawiderter <dwalczak456@gmail.com>
2022-10-07 16:20:43 +0200

    Added script for getting oh my zsh and plugins

---
+++ .setup_omz.sh
@@ -0,0 +1,9 @@
+# Oh My Zsh 
+git --version || apt install git;  
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh;
+
+# Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k;
+
+# Zsh Autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
